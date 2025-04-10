4cefc371f0266fead1e7036707d92b78fbc12515
Godspower Eze <Godspowereze260@gmail.com>
2025-04-10 02:12:33 +0100

    feat: update

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,116 @@
 mod ring;
 
+use rand::rngs::OsRng;
+use rand::RngCore;
+use sha3::{Digest, Sha3_256, Sha3_512};
+
+enum TYPE {
+    ML_KEM_512,
+    ML_KEM_768,
+    ML_KEM_1024,
+}
+
+struct MLKEM {
+    k: u8,
+    eta_1: u8,
+    eta_2: u8,
+    du: u8,
+    dv: u8,
+}
+
+impl MLKEM {
+    fn new(type_of: TYPE) -> Self {
+        match type_of {
+            TYPE::ML_KEM_512 => MLKEM {
+                k: 2,
+                eta_1: 3,
+                eta_2: 2,
+                du: 10,
+                dv: 4,
+            },
+            TYPE::ML_KEM_768 => MLKEM {
+                k: 3,
+                eta_1: 2,
+                eta_2: 2,
+                du: 10,
+                dv: 4,
+            },
+            TYPE::ML_KEM_1024 => MLKEM {
+                k: 2,
+                eta_1: 2,
+                eta_2: 2,
+                du: 11,
+                dv: 5,
+            },
+        }
+    }
+
+    fn keygen(&self) -> (String, String) {
+        let d = Self::random_bytes(32);
+        let z = Self::random_bytes(32);
+
+        // let (ek, dk) = self._keygen_internal(&d, &z);
+        todo!()
+    }
+
+    fn key_derive(&self, seed: String) -> (String, String) {
+        todo!()
+    }
+
+    fn encaps(&self, ek: String) -> (String, String) {
+        todo!()
+    }
+
+    fn decaps(&self, dk: String, c: String) -> String {
+        todo!()
+    }
+
+    // fn _keygen_internal(&self, d: &[u8], z: &[u8]) -> (&[u8], &[u8]) {}
+
+    fn _k_pke_keygen(&self, d: &[u8]) -> (String, String) {
+        let pre_image: Vec<u8> = [d, &[self.k]].concat();
+
+        let (rho, sigma) = Self::_G(&pre_image);
+
+        todo!()
+    }
+
+    fn random_bytes(length: usize) -> Vec<u8> {
+        let mut bytes = vec![0u8; length];
+        OsRng.fill_bytes(&mut bytes);
+        bytes
+    }
+
+    fn _G(s: &[u8]) -> (Vec<u8>, Vec<u8>) {
+        let mut hasher = Sha3_512::new();
+        hasher.update(s);
+        let result = hasher.finalize();
+        return (result[..32].to_vec(), result[32..].to_vec());
+    }
+
+    fn _H(s: &[u8]) -> Vec<u8> {
+        let mut hasher = Sha3_256::new();
+        hasher.update(s);
+        let result = hasher.finalize();
+        return result.to_vec();
+    }
+
+    fn _generate_matrix_from_seed(&self, rho: &[u8], transpose: bool) {}
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {}
+    fn random_bytes() {
+        let bytes = MLKEM::random_bytes(32);
+    }
+
+    #[test]
+    fn _G() {
+        let bytes = MLKEM::random_bytes(32);
+        let hash = MLKEM::_H(&bytes);
+        println!("{:?}", hash);
+    }
 }
--- src/ring.rs
+++ src/ring.rs
@@ -1,86 +1,86 @@
-use num_bigint::BigInt;
-use num_traits::{One, Zero};
+// use num_bigint::BigInt;
+// use num_traits::{One, Zero};
 
-struct PolynomialRing {
-    q: BigInt,
-    n: BigInt,
-    coefficients: Vec<BigInt>,
-}
+// struct KyberRing {
+//     q: BigInt,
+//     n: BigInt,
+//     coefficients: Vec<BigInt>,
+// }
 
-impl PolynomialRing {
-    fn new(q: BigInt, n: BigInt, coefficients: &Vec<BigInt>) -> Self {
-        PolynomialRing {
-            q,
-            n,
-            coefficients: coefficients.to_vec(),
-        }
-    }
+// impl KyberRing {
+//     fn new(q: BigInt, n: BigInt, coefficients: &Vec<BigInt>) -> Self {
+//         PolynomialRing {
+//             q,
+//             n,
+//             coefficients: coefficients.to_vec(),
+//         }
+//     }
 
-    fn is_zero(&self) -> bool {
-        // Return if the polynomial ring is zero: f = 0
-        self.coefficients.iter().all(|x| x.is_zero())
-    }
+//     fn is_zero(&self) -> bool {
+//         // Return if the polynomial ring is zero: f = 0
+//         self.coefficients.iter().all(|x| x.is_zero())
+//     }
 
-    fn is_constant(&self) -> bool {
-        // Return if the polynomial ring is constant: f = c
-        //
-        // Note: For simplicity, we don't consider zero polynomial a constant. That is [0] or [0, 0] is not a constant polynomial ring
-        if self.coefficients.is_empty() {
-            return false;
-        }
-        self.coefficients[1..].iter().all(|x| x.is_zero()) && !self.coefficients[0].is_zero()
-    }
-}
+//     fn is_constant(&self) -> bool {
+//         // Return if the polynomial ring is constant: f = c
+//         //
+//         // Note: For simplicity, we don't consider zero polynomial a constant. That is [0] or [0, 0] is not a constant polynomial ring
+//         if self.coefficients.is_empty() {
+//             return false;
+//         }
+//         self.coefficients[1..].iter().all(|x| x.is_zero()) && !self.coefficients[0].is_zero()
+//     }
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn new() {
-        let q = BigInt::from(7);
-        let n = BigInt::from(256);
-        let coefficients = vec![BigInt::from(1), BigInt::from(2)];
-        let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
-        assert_eq!(poly_ring.q, q);
-        assert_eq!(poly_ring.n, n);
-        assert_eq!(poly_ring.coefficients, coefficients);
-    }
+//     #[test]
+//     fn new() {
+//         let q = BigInt::from(7);
+//         let n = BigInt::from(256);
+//         let coefficients = vec![BigInt::from(1), BigInt::from(2)];
+//         let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
+//         assert_eq!(poly_ring.q, q);
+//         assert_eq!(poly_ring.n, n);
+//         assert_eq!(poly_ring.coefficients, coefficients);
+//     }
 
-    #[test]
-    fn is_zero() {
-        let q = BigInt::from(7);
-        let n = BigInt::from(256);
-        let coefficients = vec![BigInt::from(1), BigInt::from(2)];
-        let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
-        assert!(!poly_ring.is_zero());
-        let coefficients = vec![BigInt::zero(), BigInt::from(2)];
-        let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
-        assert!(!poly_ring.is_zero());
-        let coefficients = vec![BigInt::zero(), BigInt::zero()];
-        let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
-        assert!(poly_ring.is_zero());
-        let poly_ring = PolynomialRing::new(q, n, &vec![]);
-        assert!(poly_ring.is_zero());
-    }
+//     #[test]
+//     fn is_zero() {
+//         let q = BigInt::from(7);
+//         let n = BigInt::from(256);
+//         let coefficients = vec![BigInt::from(1), BigInt::from(2)];
+//         let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
+//         assert!(!poly_ring.is_zero());
+//         let coefficients = vec![BigInt::zero(), BigInt::from(2)];
+//         let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
+//         assert!(!poly_ring.is_zero());
+//         let coefficients = vec![BigInt::zero(), BigInt::zero()];
+//         let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
+//         assert!(poly_ring.is_zero());
+//         let poly_ring = PolynomialRing::new(q, n, &vec![]);
+//         assert!(poly_ring.is_zero());
+//     }
 
-    #[test]
-    fn is_constant() {
-        let q = BigInt::from(7);
-        let n = BigInt::from(256);
-        let coefficients = vec![BigInt::from(1), BigInt::from(2)];
-        let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
-        assert!(!poly_ring.is_constant());
-        let coefficients = vec![BigInt::zero(), BigInt::from(2)];
-        let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
-        assert!(!poly_ring.is_constant());
-        let coefficients = vec![BigInt::zero(), BigInt::zero()];
-        let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
-        assert!(!poly_ring.is_constant());
-        let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &vec![]);
-        assert!(!poly_ring.is_constant());
-        let coefficients = vec![BigInt::one(), BigInt::zero()];
-        let poly_ring = PolynomialRing::new(q, n, &coefficients);
-        assert!(poly_ring.is_constant());
-    }
-}
+//     #[test]
+//     fn is_constant() {
+//         let q = BigInt::from(7);
+//         let n = BigInt::from(256);
+//         let coefficients = vec![BigInt::from(1), BigInt::from(2)];
+//         let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
+//         assert!(!poly_ring.is_constant());
+//         let coefficients = vec![BigInt::zero(), BigInt::from(2)];
+//         let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
+//         assert!(!poly_ring.is_constant());
+//         let coefficients = vec![BigInt::zero(), BigInt::zero()];
+//         let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &coefficients);
+//         assert!(!poly_ring.is_constant());
+//         let poly_ring = PolynomialRing::new(q.clone(), n.clone(), &vec![]);
+//         assert!(!poly_ring.is_constant());
+//         let coefficients = vec![BigInt::one(), BigInt::zero()];
+//         let poly_ring = PolynomialRing::new(q, n, &coefficients);
+//         assert!(poly_ring.is_constant());
+//     }
+// }
